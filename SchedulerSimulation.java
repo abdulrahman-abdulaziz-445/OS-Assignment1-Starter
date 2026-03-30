b210bdcce5d6f3f24385fdad77bc591f0a0c0885
abdulrahman-abdulaziz-445 <445050091@std.psau.edu.sa>
2026-03-30 15:27:10 +0300

    add a comment

--- SchedulerSimulation.java
+++ SchedulerSimulation.java
@@ -22,7 +22,7 @@ class Colors {
     public static final String BRIGHT_CYAN = "\u001B[96m";
     public static final String BRIGHT_YELLOW = "\u001B[93m";
     public static final String BRIGHT_GREEN = "\u001B[92m";
-    public static final String BRIGHT_MAGENTA = "\u001B[95m";
+    public static final String BRIGHT_MAGENTA = "\u001B[95m"; // the color used for the summary table header and footer
 }
 
 // Class representing a process that implements Runnable to be run by a thread
